6842594cab24f17b4aa020c425c444521e2988ab
vladis18 <vladisbabayan@gmail.com>
2019-12-08 11:13:06 -0500

    loop code

--- src/Whileloop.java
+++ src/Whileloop.java
@@ -2,16 +2,15 @@
 public class Whileloop {
 
 	public static void main(String[] args) {
-		int counter=0;
-		 
-		 
-		for (int i =counter; i<= 5; i++) {
-			 for (int j = 1; j<=i*10 ;j ++) {
-				 System.out.print(j*counter);
-			 }
-			 
-			 System.out.println();
-			 counter=counter+1;
+		int counter = 0;
+
+		for (int i = counter; i <= 5; i++) {
+			for (int j = 1; j <= i * 10; j++) {
+				System.out.print(j * counter);
+			}
+
+			System.out.println("Hello");
+			counter = counter + 1;
 			// System.out.print(i );
 		}
 
